f5266ae0d8c415a6765446ab6085fdc5a9655671
Diego Javier Zea <diegozea@gmail.com>
2023-02-08 12:52:09 +0100

    add a Python wrapper for the R script

--- src/aspring/step_06_stats.py
+++ src/aspring/step_06_stats.py
@@ -1,31 +1,69 @@
 """ 
 This scripts runs the step_06_getStats.R script 
-"""         
+"""
 import os
 import sys
 import subprocess
+import argparse
+
+from aspring import __version__
+
+
+def test_rscript_availability():
+    try:
+        cmd = ["Rscript", "--version"]
+        subprocess.run(cmd,
+                       check=True,
+                       stdout=subprocess.DEVNULL,
+                       stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        raise Exception(
+            "Rscript not found. Please install R and make sure Rscript is in your PATH."
+        )
+
+
+def get_package_path():
+    aspring_path = os.path.dirname(os.path.abspath(__file__))
+    package_path = os.path.join(aspring_path, "..", "..")
+    return package_path
+
+
+def run_r_script(gene, path_data):
+    test_rscript_availability()
+    package_path = get_package_path()
+    r_script_file = os.path.join(package_path, "src/aspring/R_script",
+                                 "step_06_getStats.R")
+    # Initialize the R environment
+    subprocess.run(
+        ["Rscript", "-e", "if (!require('renv')) install.packages('renv')"],
+        check=True)
+    subprocess.run(
+        ["Rscript", "-e", "renv::restore(project='" + package_path + "')"],
+        check=True)
+    # Run the R script
+    subprocess.run(
+        ["Rscript", r_script_file, "--gene", gene, "--path_data", path_data],
+        check=True)
+
 
 def parse_args(args):
-    import argparse
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--gene', required=True, help='gene name')
-    parser.add_argument('--out', required=True, help='output directory')
-    parser.add_argument('--rscript', required=True, help='Rscript path')
-
+    parser = argparse.ArgumentParser(description="Get stats for a gene")
+    parser.add_argument('--gene', required=True, help='Gene name')
+    parser.add_argument('--path_data',
+                        required=True,
+                        help='Path to dir containing Thoraxe outputs')
+    parser.add_argument('--version',
+                        action='version',
+                        version=f"aspring {__version__}")
     return parser.parse_args(args)
 
-def main(args):
-    args = parse_args(args)
+
+def run():
+    args = parse_args(sys.argv[1:])
     gene = args.gene
-    out = args.out
-    rscript = args.rscript
+    path_data = args.path_data
+    run_r_script(gene, path_data)
 
-    # run R script
-    cmd = [rscript, 'step_06_getStats.R', gene, out]
-    subprocess.call(cmd)
-    
+
 if __name__ == '__main__':
-    main(sys.argv[1:])
-
-
-
+    run()
