0cfd655aedcc3025358f52b9b5dd47a8723914bb
Diego Javier Zea <diegozea@gmail.com>
2023-04-28 10:21:04 +0200

    Add tests for step 10

--- tests/test_aspring.py
+++ tests/test_aspring.py
@@ -1,3 +1,4 @@
+import warnings
 import pytest
 import os
 import pandas as pd
@@ -31,15 +32,16 @@ def setup_and_run_pipeline(request, clean_up):
         # Local
         '/home/diego/bin/miniconda3/scripts',
         # GitHUb action CI
-        '/usr/share/miniconda/envs/test/scripts', 
+        '/usr/share/miniconda/envs/test/scripts',
         '/usr/local/miniconda/envs/test/scripts']
     for path in possible_paths:
         if os.path.isdir(path):
             path_hhsuite_scripts = path
             break
     if path_hhsuite_scripts is None:
-        raise Exception('path_hhsuite_scripts is None: please set it manually to test aspring in your machine')
-    
+        raise Exception(
+            'path_hhsuite_scripts is None: please set it manually to test aspring in your machine')
+
     gene = 'ENSG00000007866'
     msa_len = 5
     msa_id_threshold = 100
@@ -95,8 +97,8 @@ def test_events(setup_and_run_pipeline):
     ref = os.path.join(path_ref, filename)
 
     columns_to_compare = ['gene', 'rank', 'type', 'lePathA', 'lePathB', 'exclu',
-                            'ncols', 'leA', 'leB', 'typePair', 'ColA', 'ColB']
-    
+                          'ncols', 'leA', 'leB', 'typePair', 'ColA', 'ColB']
+
     df_out = pd.read_csv(out, index_col=False)
     df_ref = pd.read_csv(ref, index_col=False)
 
@@ -141,3 +143,61 @@ def test_asrus(setup_and_run_pipeline):
 
     assert (df_out[columns_to_compare] == df_ref[columns_to_compare]
             ).all().sum() == len(columns_to_compare)
+
+
+def is_pdb(path):
+    """Check if the file at the given path is a PDB file."""
+    pdb_keywords = ['HEADER', 'OBSLTE', 'TITLE', 'SPLT', 'CAVEAT', 'COMPND', 'SOURCE',
+                    'KEYWDS', 'EXPDTA', 'NUMMDL', 'MDLTYP', 'AUTHOR', 'REVDAT', 'SPRSDE',
+                    'JRNL', 'REMARK', 'DBREF', 'DBREF1', 'DBREF2', 'SEQADV', 'SEQRES',
+                    'MODRES', 'HET', 'FORMUL', 'HETNAM', 'HETSYN', 'HELIX', 'SHEET',
+                    'SSBOND', 'LINK', 'CISPEP', 'SITE', 'CRYST1', 'MTRIX1', 'MTRIX2',
+                    'MTRIX3', 'ORIGX1', 'ORIGX2', 'ORIGX3', 'SCALE1', 'SCALE2', 'SCALE3',
+                    'MODEL', 'ATOM', 'ANISOU', 'TER', 'HETATM', 'ENDMDL', 'CONECT',
+                    'MASTER', 'END']
+    with open(path, 'r') as f:
+        for line in f:
+            if not line.startswith(tuple(pdb_keywords)):
+                print(f'The line that failed is: {line}')
+                return False
+    return True
+
+
+def is_pml(path):
+    """Check if the file at the given path is a PyMol script."""
+    # list of the PyMol commands that are used in the write_pml function
+    pml_keywords = ['load', 'bg_color', 'color', 'select', 'join', 'deselect']
+    with open(path, 'r') as f:
+        for line in f:
+            if not line.startswith(tuple(pml_keywords)):
+                return False
+    return True
+
+
+def test_structures_folder(setup_and_run_pipeline):
+    path_gene = setup_and_run_pipeline['path_gene']
+    structures_folder = os.path.join(path_gene, 'structures')
+    assert os.path.isdir(structures_folder)
+
+    files = os.listdir(structures_folder)
+    if files:
+        for file in files:
+            # test that the file has the right extension
+            assert file.endswith('.pdb') or file.endswith('.pml')
+            # test that the file is not empty
+            assert os.path.getsize(os.path.join(structures_folder, file)) > 0
+            # test that the file has the right name
+            if file.endswith('.pdb'):
+                assert file.startswith('AF-')
+                assert file.endswith('-F1-model_v4.pdb')
+                # test that the file is a proper PDB file
+                assert is_pdb(os.path.join(structures_folder, file))
+            if file.endswith('.pml'):
+                assert '_AF-' in file
+                assert file.endswith('-F1-model_v4.pml')
+                # test that the file is a proper PyMol script
+                assert is_pml(os.path.join(structures_folder, file))
+    else:
+        # warn the user that no structure was generated withouth failing the test
+        with pytest.warns(UserWarning):
+            warnings.warn('No structures were downloaded for this gene.')
